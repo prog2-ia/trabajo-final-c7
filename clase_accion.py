2accd6c093f8318c4f477dd383750b4f677589fe
Henry-263 <henrysanchezjuan@gmail.com>
2026-02-27 14:05:11 +0100

    Actualizamos la clase accion

--- clase_accion.py
+++ clase_accion.py
@@ -1,4 +1,12 @@
 class Accion():
-    def __init__(self, simbolo, precio):
-        self.simbolo = simbolo
+    def __init__(self, id, nombre, precio):
+        self.id = id
+        self.nombre = nombre
         self.precio = precio
+
+    def __str__(self):
+        return (
+            f'Id de la accion: {self.id}\n'
+            f'Nombre: {self.nombre}\n'
+            f'Precio: {self.precio}\n')
+
