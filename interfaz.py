b406325e8cdf6730841629b25f96b76c741afe14
ismael <ifb18@alu.ua.es>
2026-03-06 14:01:19 +0100

    Interfaz base

--- interfaz.py
+++ interfaz.py
@@ -2,12 +2,47 @@ from clase_transaccion import Transaccion
 from clase_usuario import Usuario
 
 class Interfaz:
-    def __init__(self,usuario,simulador,estado_actual,transacciones):
-        self.estado_actual = estado_actual
-        self.transacciones = transacciones
-        self.usuario = usuario
-        self.estado_actual = estado_actual
-        self.simulador = simulador
-
     def mostrar_menu(self):
+
+            print("----- simulador de inversion -----")
+            print("1 ver usuario")
+            print("2 comprar accion")
+            print("3 vender accion")
+            print("4 ver cartera")
+            print("5 ver transacciones")
+            print("0 salir")
+
+            opcion = input("elige opcion: ")
+
+            if opcion == "1":
+                self.ver_usuario()
+            elif opcion == "2":
+                pass
+                #self.comprar()
+            elif opcion == "3":
+                pass
+                #self.vender()
+            elif opcion == "4":
+                self.ver_cartera()
+            elif opcion == "5":
+                self.ver_transacciones()
+            elif opcion == "0":
+                self.salir()
+            else:
+                print("opcion no valida")
+
+    def salir(self):
+        return False
+
+    def ver_usuario(self):
         pass
+
+
+    def ver_cartera(self):
+        pass
+
+
+    def ver_transacciones(self):
+        pass
+
+
